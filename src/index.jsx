1c4540e3551f8c46492c0a3c40f565cbc9fce0b1
llama-ste <andong1212@gmail.com>
2022-11-25 21:55:39 +0900

    fix: 최상위 index.jsx에 provider및 router설정

---
+++ src/index.jsx
@@ -0,0 +1,89 @@
+import "react-toastify/dist/ReactToastify.css";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Reset } from "styled-reset";
+import { CookiesProvider } from "react-cookie";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RecoilRoot } from "recoil";
+import { ToastContainer } from "react-toastify";
+
+import "./index.css";
+import App from "./App";
+import Home from "./pages/Home";
+import theme from "./styles/theme";
+import SignIn from "./pages/SignIn";
+import NewProduct from "./pages/NewProduct";
+import Category from "./pages/Category";
+import AdminHome from "./pages/AdminHome";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const options = {
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+};
+
+const toastConfig = {
+  autoClose: 500,
+  bodyStyle: { fontSize: "14px" },
+  closeButton: true,
+  closeOnClick: true,
+  limit: 1,
+  pauseOnHover: false,
+  position: "top-center",
+  progressStyle: {
+    background: `linear-gradient(
+      to right,
+      #4cd964,
+      #5ac8fa,
+      #007aff,
+      #34aadc,
+      #5856d6,
+      #ff2d55
+    )`,
+  },
+  style: {
+    maxWidth: "1000px",
+    width: "max-content",
+    zIndex: 1000000,
+  },
+  theme: "light",
+};
+
+const queryClient = new QueryClient(options);
+
+root.render(
+  <CookiesProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <RecoilRoot>
+          <Reset />
+          <CssBaseline />
+          <ToastContainer {...toastConfig} />
+          <Router>
+            <Routes>
+              <Route path="/" element={<App />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/categories/:categoryId" element={<Category />} />
+                <Route path="/admin" element={<AdminHome />} />
+                <Route
+                  path="/admin/categories/:categoryId"
+                  element={<Category />}
+                />
+              </Route>
+              <Route path="/admin/sign-in" element={<SignIn />} />
+              <Route path="/admin/new-product" element={<NewProduct />} />
+            </Routes>
+          </Router>
+        </RecoilRoot>
+      </ThemeProvider>
+    </QueryClientProvider>
+  </CookiesProvider>
+);
