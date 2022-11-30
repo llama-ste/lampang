fadd5995a702a57f3e84332ad4b854a2e20dbc78
llama-ste <andong1212@gmail.com>
2022-11-30 19:33:04 +0900

    fix: 토큰 만료시 로그아웃

--- src/api/client.js
+++ src/api/client.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { deleteCookie } from "../common/cookie";
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
@@ -7,5 +8,16 @@ client.defaults.baseURL = API_BASE_URL;
 client.defaults.headers = {
   "Content-Type": "application/json",
 };
+
+client.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err.response.status === 401) {
+      deleteCookie("token");
+      deleteCookie("refreshToken");
+      deleteCookie("username");
+    }
+  }
+);
 
 export default client;
